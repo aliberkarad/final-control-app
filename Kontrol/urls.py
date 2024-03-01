37a01fcb515b20f200da6065f8341b3bd19819c3
Berkay Karadeniz <elektronik@5kkaravan.com>
2024-03-01 16:49:11 +0300

    login page added

--- Kontrol/urls.py
+++ Kontrol/urls.py
@@ -3,5 +3,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.main,name='main'),
+    path('',views.login,name='login'),
+    path('main',views.main,name='main'),
 ]
--- Kontrol/views.py
+++ Kontrol/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    return render(request,"blog/login.html")
 
 def main(request):
     return render(request,"blog/index.html")
