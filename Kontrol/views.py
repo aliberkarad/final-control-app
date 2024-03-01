360b29072cd149483126d25bb846819663415b54
Berkay Karadeniz <elektronik@5kkaravan.com>
2024-03-02 00:43:12 +0300

    updated

--- Kontrol/views.py
+++ Kontrol/views.py
@@ -27,28 +27,35 @@ def home(request):
     if request.user.is_authenticated == False:
         return redirect('login')
     
+    ayaz_models = ['AYAZ SMART',
+                    'AYAZ COMFORT',
+                    'AYAZ PREMIUM',
+                    'AYAZ OFFROAD SMART',
+                    'AYAZ OFFROAD COMFORT',
+                    'AYAZ OFFROAD PREMIUM',]
+        
+    arkut_models = ['ARKUT SMART',
+                    'ARKUT COMFORT',
+                    'ARKUT PREMIUM']
+    
+    alada_models = ['ALADA PREMIUM',]
+    
+    gredia_models = ['GREDIA PREMIUM',]
+
+    ayazlar = KontrolClass.objects.filter(model__contains="AYAZ")
+    arkutlar = KontrolClass.objects.filter(model__contains="ARKUT")
+    aladalar = KontrolClass.objects.filter(model__contains="ALADA")
+    gredialar = KontrolClass.objects.filter(model__contains="GREDIA")
+
     if request.method == "POST":
         date = request.POST.get('date')
         model = request.POST.get('model')
         chassis = request.POST.get('chassis')
 
-        ayaz_models = ['AYAZ SMART',
-                       'AYAZ COMFORT',
-                       'AYAZ PREMIUM',
-                       'AYAZ OFFROAD SMART',
-                       'AYAZ OFFROAD COMFORT',
-                       'AYAZ OFFROAD PREMIUM',]
-        
-        arkut_models = ['ARKUT SMART',
-                        'ARKUT COMFORT',
-                        'ARKUT PREMIUM']
-        
-        alada_models = ['ALADA PREMIUM',]
-        
-        gredia_models = ['GREDIA PREMIUM',]
+        caravan = KontrolClass.objects.create(date=date,model=model,chassis=chassis)
 
         if model in ayaz_models:
-            pass
+            return redirect('ayaz')
 
         if model in arkut_models:
             pass
@@ -59,7 +66,12 @@ def home(request):
         if model in gredia_models:
             pass
 
-    return render(request,"blog/home.html")
+    return render(request,"blog/home.html",{ 
+        'ayazlar' : ayazlar,
+        'arkutlar' : arkutlar,
+        'aladalar' : aladalar,
+        'gredialar' : gredialar,
+    })
 
 def ayaz(request):
     if request.user.is_authenticated == False:
